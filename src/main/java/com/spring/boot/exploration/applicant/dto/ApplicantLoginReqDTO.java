af18ad98d96ab7d210756b6b7cc5d19e505351db
subha92sde <subha92sde@gmail.com>
2024-12-01 18:16:55 +0600

    -_-

--- src/main/java/com/spring/boot/exploration/applicant/dto/ApplicantLoginReqDTO.java
+++ src/main/java/com/spring/boot/exploration/applicant/dto/ApplicantLoginReqDTO.java
@@ -1,5 +1,6 @@
 package com.spring.boot.exploration.applicant.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -12,9 +13,11 @@ public class ApplicantLoginReqDTO {
     @NotNull
     @Pattern(regexp = "\\d{3}-\\d{2}-\\d{4}", message = "Invalid SSN format")
     @Size(min = 11, max = 11, message = "SSN must be exactly 11 characters long.")
+    @JsonProperty
     private String ssn;
 
     @NotNull
     @Pattern(regexp = "^[a-zA-Z0-9]{5,10}$", message = "Password must be 5-10 characters long and contain only letters and digits.")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 }
