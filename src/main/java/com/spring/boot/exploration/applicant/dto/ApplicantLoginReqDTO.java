e1d2e5beb27c6e15b2bd45583f453863b98c8ecf
subha92sde <subha92sde@gmail.com>
2024-12-01 17:38:50 +0600

    -_-

---
+++ src/main/java/com/spring/boot/exploration/applicant/dto/ApplicantLoginReqDTO.java
@@ -0,0 +1,20 @@
+package com.spring.boot.exploration.applicant.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApplicantLoginReqDTO {
+    @NotNull
+    @Pattern(regexp = "\\d{3}-\\d{2}-\\d{4}", message = "Invalid SSN format")
+    @Size(min = 11, max = 11, message = "SSN must be exactly 11 characters long.")
+    private String ssn;
+
+    @NotNull
+    @Pattern(regexp = "^[a-zA-Z0-9]{5,10}$", message = "Password must be 5-10 characters long and contain only letters and digits.")
+    private String password;
+}
